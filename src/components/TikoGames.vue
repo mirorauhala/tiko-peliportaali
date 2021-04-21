1e0e9dc903090b6a9dc0cf19e4daca9992443729
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-22 00:54:07 +0300

    use higher breakpoint

--- src/components/TikoGames.vue
+++ src/components/TikoGames.vue
@@ -13,15 +13,15 @@ export default {
 
 <template>
   <section class="max-w-7xl mx-auto mb-12">
-    <div class="px-1.5 md:px-0 mb-3 md:mb-5">
+    <div class="px-1.5 xl:px-0 mb-3 md:mb-5">
       <h1 class="text-4xl font-bold mb-5 text-center">Pelit</h1>
       <input
         type="text"
-        class="text-xl px-6 w-full h-20 border-4 border-opacity-20 border-black transition delay-100
+        class="text-lg md:text-xl px-3 md:px-6 w-full h-16 md:h-20 border-4 border-opacity-20 border-black transition delay-100
         ease-out focus:text-tuni focus:outline-none focus:border-tuni" placeholder="Hae pelejä...">
     </div>
 
-    <div class="md:-m-1.5">
+    <div class="xl:-m-1.5">
 
       <div class="flex flex-wrap justify-center">
         <article
