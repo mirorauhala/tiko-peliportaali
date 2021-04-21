8875e8f56a02d9fd3eb57ce53fa3dd21474e8bdc
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-21 16:00:17 +0300

    loop over the games

--- src/components/TikoGames.vue
+++ src/components/TikoGames.vue
@@ -1,6 +1,126 @@
 <script>
 export default {
-  name: 'TikoGames',
+  team: 'TikoGames',
+  data() {
+    return {
+      games: [
+        {
+          id: 1,
+          name: 'The name of the game goes here',
+          team: '20tiko1a',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 2,
+          name: 'The name of the game goes here',
+          team: '20tiko1b',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 3,
+          name: 'The name of the game goes here',
+          team: '20tiko1c',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 4,
+          name: 'The name of the game goes here',
+          team: '20tiko1d',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 5,
+          name: 'The name of the game goes here',
+          team: '20tiko1e',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 6,
+          name: 'The name of the game goes here',
+          team: '20tiko1f',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 7,
+          name: 'The name of the game goes here',
+          team: '20tiko2a',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 8,
+          name: 'The name of the game goes here',
+          team: '20tiko2b',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 9,
+          name: 'The name of the game goes here',
+          team: '20tiko2c',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 10,
+          name: 'The name of the game goes here',
+          team: '20tiko2d',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 11,
+          name: 'The name of the game goes here',
+          team: '20tiko2e',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 12,
+          name: 'The name of the game goes here',
+          team: '20tiko2f',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 13,
+          name: 'The name of the game goes here',
+          team: '20tiko2g',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 14,
+          name: 'The name of the game goes here',
+          team: '20tiko3a',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 15,
+          name: 'The name of the game goes here',
+          team: '20tiko3b',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 16,
+          name: 'The name of the game goes here',
+          team: '20tiko3c',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 17,
+          name: 'The name of the game goes here',
+          team: '20tiko3d',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 18,
+          name: 'The name of the game goes here',
+          team: '20tiko3e',
+          image: 'http://placekitten.com/1080/1080',
+        },
+        {
+          id: 19,
+          name: 'The name of the game goes here',
+          team: '20tiko3f',
+          image: 'http://placekitten.com/1080/1080',
+        },
+      ]
+    }
+  }
 };
 </script>
 
@@ -16,36 +136,19 @@ export default {
 
     <div class="-m-1.5">
 
-      <div class="flex">
-        <article class="w-1/3">
+      <div class="flex flex-wrap justify-center">
+        <article
+          class="w-1/3"
+          v-for="game in this.games">
           <a class="block p-1.5" href="#" target="_blank">
             <div class="mb-2">
-              <img src="http://placekitten.com/1080/1080">
+              <img :src="game.image" alt="">
             </div>
-            <h1 class="text-xl font-bold tracking-tight">The name of the game goes here</h1>
-            <p class="uppercase opacity-80 tracking-tight">tiko20a</p>
+            <h1 class="text-xl font-bold tracking-tight" v-text="game.name"></h1>
+            <p class="uppercase opacity-80 tracking-tight" v-text="game.team"></p>
           </a>
         </article>
 
-        <article class="w-1/3">
-          <a class="block p-1.5" href="#" target="_blank">
-            <div class="mb-2">
-              <img src="http://placekitten.com/1080/1080">
-            </div>
-            <h1 class="text-xl font-bold tracking-tight">The name of the game goes here</h1>
-            <p class="uppercase opacity-80 tracking-tight">tiko20a</p>
-          </a>
-        </article>
-
-        <article class="w-1/3">
-          <a class="block p-1.5" href="#" target="_blank">
-            <div class="mb-2">
-              <img src="http://placekitten.com/1080/1080">
-            </div>
-            <h1 class="text-xl font-bold tracking-tight">The name of the game goes here</h1>
-            <p class="uppercase opacity-80 tracking-tight">tiko20a</p>
-          </a>
-        </article>
       </div>
     </div>
   </section>
