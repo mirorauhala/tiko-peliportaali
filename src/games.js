5962a2952028f1e074dab4cb97c9c13439862bc1
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-21 23:03:40 +0300

    add logos & load games from files

---
+++ src/games.js
@@ -0,0 +1,136 @@
+import tiko1a from './assets/img/games/20tiko1a.png';
+import tiko1b from './assets/img/games/20tiko1b.png';
+import tiko1c from './assets/img/games/20tiko1c.png';
+import tiko1d from './assets/img/games/20tiko1d.png';
+import tiko1e from './assets/img/games/20tiko1e.png';
+import tiko1f from './assets/img/games/20tiko1f.png';
+import tiko2a from './assets/img/games/20tiko2a.png';
+import tiko2b from './assets/img/games/20tiko2b.png';
+import tiko2c from './assets/img/games/20tiko2c.png';
+//import tiko2d from './assets/img/games/20tiko2d.png';
+import tiko2e from './assets/img/games/20tiko2e.png';
+import tiko2f from './assets/img/games/20tiko2f.png';
+import tiko3a from './assets/img/games/20tiko3a.png';
+import tiko3b from './assets/img/games/20tiko3b.png';
+import tiko3c from './assets/img/games/20tiko3c.png';
+import tiko3d from './assets/img/games/20tiko3d.png';
+import tiko3e from './assets/img/games/20tiko3e.png';
+import tiko3f from './assets/img/games/20tiko3f.png';
+
+
+export default [
+    {
+      id: 1,
+      name: 'The name of the game goes here',
+      team: '20tiko1a',
+      image: tiko1a,
+    },
+    {
+      id: 2,
+      name: 'The name of the game goes here',
+      team: '20tiko1b',
+      image: tiko1b,
+    },
+    {
+      id: 3,
+      name: 'The name of the game goes here',
+      team: '20tiko1c',
+      image: tiko1c,
+    },
+    {
+      id: 4,
+      name: 'The name of the game goes here',
+      team: '20tiko1d',
+      image: tiko1d,
+    },
+    {
+      id: 5,
+      name: 'The name of the game goes here',
+      team: '20tiko1e',
+      image: tiko1e,
+    },
+    {
+      id: 6,
+      name: 'The name of the game goes here',
+      team: '20tiko1f',
+      image: tiko1f,
+    },
+    {
+      id: 7,
+      name: 'The name of the game goes here',
+      team: '20tiko2a',
+      image: tiko2a,
+    },
+    {
+      id: 8,
+      name: 'Bloom or Gloom',
+      team: '20tiko2b',
+      image: tiko2b,
+    },
+    {
+      id: 9,
+      name: 'The name of the game goes here',
+      team: '20tiko2c',
+      image: tiko2c,
+    },
+    {
+      id: 10,
+      name: 'The name of the game goes here',
+      team: '20tiko2d',
+      image: tiko2c,
+    },
+    {
+      id: 11,
+      name: 'The name of the game goes here',
+      team: '20tiko2e',
+      image: tiko2e,
+    },
+    {
+      id: 12,
+      name: 'The name of the game goes here',
+      team: '20tiko2f',
+      image: tiko2f,
+    },
+    {
+      id: 13,
+      name: 'The name of the game goes here',
+      team: '20tiko2g',
+      image: tiko2f,
+    },
+    {
+      id: 14,
+      name: 'The name of the game goes here',
+      team: '20tiko3a',
+      image: tiko3a,
+    },
+    {
+      id: 15,
+      name: 'The name of the game goes here',
+      team: '20tiko3b',
+      image: tiko3b,
+    },
+    {
+      id: 16,
+      name: 'The name of the game goes here',
+      team: '20tiko3c',
+      image: tiko3c,
+    },
+    {
+      id: 17,
+      name: 'The name of the game goes here',
+      team: '20tiko3d',
+      image: tiko3d,
+    },
+    {
+      id: 18,
+      name: 'The name of the game goes here',
+      team: '20tiko3e',
+      image: tiko3e,
+    },
+    {
+      id: 19,
+      name: 'The name of the game goes here',
+      team: '20tiko3f',
+      image: tiko3f,
+    },
+]
