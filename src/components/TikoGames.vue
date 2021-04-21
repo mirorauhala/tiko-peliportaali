0545dc0ca40d9a02c91783c363e0b42f9b51879e
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-22 00:21:27 +0300

    fix styles

--- src/components/TikoGames.vue
+++ src/components/TikoGames.vue
@@ -1,5 +1,6 @@
 <script>
 import games from '../games';
+
 export default {
   name: 'TikoGames',
   data() {
@@ -12,7 +13,7 @@ export default {
 
 <template>
   <section class="max-w-7xl mx-auto mb-12">
-    <div class="mb-3">
+    <div class="px-1.5 md:px-0 mb-3 md:mb-5">
       <h1 class="text-4xl font-bold mb-5 text-center">Pelit</h1>
       <input
         type="text"
@@ -20,16 +21,14 @@ export default {
         ease-out focus:text-tuni focus:outline-none focus:border-tuni" placeholder="Hae pelejä...">
     </div>
 
-    <div class="-m-1.5">
+    <div class="md:-m-1.5">
 
       <div class="flex flex-wrap justify-center">
         <article
-          class="w-full md:w-1/3"
+          class="w-full md:w-1/3 mb-3 md:mb-5 hover:text-tuni"
           v-for="game in this.games">
           <a class="block p-1.5" href="#" target="_blank">
-            <div class="mb-2">
-              <img :src="game.image" alt="">
-            </div>
+            <img :src="game.image" class="mb-2" alt="">
             <h1 class="text-xl font-bold tracking-tight" v-text="game.name"></h1>
             <p class="uppercase opacity-80 tracking-tight" v-text="game.team"></p>
           </a>
