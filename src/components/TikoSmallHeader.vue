1d71bb138aa3d09b9105dfd32a1486839545641a
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-05-04 23:38:20 +0300

    finish navigation

--- src/components/TikoSmallHeader.vue
+++ src/components/TikoSmallHeader.vue
@@ -26,7 +26,7 @@ export default {
 </style>
 
 <template>
-  <header class="bg-gradient p-3 pt-10 md:py-20 mb-16">
+  <header class="bg-gradient p-3 pt-10 md:py-20 mb-10">
     <div class="flex flex-col max-w-7xl mx-auto items-center">
       <div class="w-full md:w-2/3">
         <h1 class="heading" v-text="messages[language].heading"></h1>
