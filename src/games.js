c657ec46e539dfd65cf6db280b5ad634afbc2638
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-05-06 13:36:19 +0300

    add categories to themes

--- src/games.js
+++ src/games.js
@@ -4,6 +4,7 @@ import tiko1c from './assets/img/games/20tiko1c.png';
 import tiko1d from './assets/img/games/20tiko1d.png';
 import tiko1e from './assets/img/games/20tiko1e.png';
 import tiko1f from './assets/img/games/20tiko1f.png';
+
 import tiko2a from './assets/img/games/20tiko2a.png';
 import tiko2b from './assets/img/games/20tiko2b.png';
 import tiko2c from './assets/img/games/20tiko2c.png';
@@ -11,6 +12,7 @@ import tiko2d from './assets/img/games/20tiko2d.png';
 import tiko2e from './assets/img/games/20tiko2e.png';
 import tiko2f from './assets/img/games/20tiko2f.png';
 import tiko2g from './assets/img/games/20tiko2g.png';
+
 import tiko3a from './assets/img/games/20tiko3a.png';
 import tiko3b from './assets/img/games/20tiko3b.png';
 import tiko3c from './assets/img/games/20tiko3c.png';
@@ -27,6 +29,7 @@ export default [
     team: '20tiko1a',
     link: 'https://webpages.tuni.fi/20tiko1a',
     image: tiko1a,
+    cat: 1,
   },
   {
     id: 2,
@@ -34,6 +37,7 @@ export default [
     team: '20tiko1b',
     link: 'https://webpages.tuni.fi/20tiko1b',
     image: tiko1b,
+    cat: 1,
   },
   {
     id: 3,
@@ -41,6 +45,7 @@ export default [
     team: '20tiko1c',
     link: 'https://webpages.tuni.fi/20tiko1c',
     image: tiko1c,
+    cat: 1,
   },
   {
     id: 4,
@@ -48,6 +53,7 @@ export default [
     team: '20tiko1d',
     link: 'https://webpages.tuni.fi/20tiko1d',
     image: tiko1d,
+    cat: 1,
   },
   {
     id: 5,
@@ -55,6 +61,7 @@ export default [
     team: '20tiko1e',
     link: 'https://webpages.tuni.fi/20tiko1e',
     image: tiko1e,
+    cat: 1,
   },
   {
     id: 6,
@@ -62,6 +69,7 @@ export default [
     team: '20tiko1f',
     link: 'https://webpages.tuni.fi/20tiko1f',
     image: tiko1f,
+    cat: 1,
   },
   {
     id: 7,
@@ -69,6 +77,7 @@ export default [
     team: '20tiko2a',
     link: 'https://webpages.tuni.fi/20tiko2a',
     image: tiko2a,
+    cat: 2
   },
   {
     id: 8,
@@ -76,6 +85,7 @@ export default [
     team: '20tiko2b',
     link: 'https://webpages.tuni.fi/20tiko2b',
     image: tiko2b,
+    cat: 2
   },
   {
     id: 9,
@@ -83,6 +93,7 @@ export default [
     team: '20tiko2c',
     link: 'https://webpages.tuni.fi/20tiko2c',
     image: tiko2c,
+    cat: 2
   },
   {
     id: 10,
@@ -90,6 +101,7 @@ export default [
     team: '20tiko2d',
     link: 'https://webpages.tuni.fi/20tiko2d',
     image: tiko2d,
+    cat: 2
   },
   {
     id: 11,
@@ -97,6 +109,7 @@ export default [
     team: '20tiko2e',
     link: 'https://webpages.tuni.fi/20tiko2e',
     image: tiko2e,
+    cat: 2
   },
   {
     id: 12,
@@ -104,6 +117,7 @@ export default [
     team: '20tiko2f',
     link: 'https://webpages.tuni.fi/20tiko2f',
     image: tiko2f,
+    cat: 2
   },
   {
     id: 13,
@@ -111,6 +125,7 @@ export default [
     team: '20tiko2g',
     link: 'https://webpages.tuni.fi/20tiko2g',
     image: tiko2g,
+    cat: 2
   },
   {
     id: 14,
@@ -118,6 +133,7 @@ export default [
     team: '20tiko3a',
     link: 'https://webpages.tuni.fi/20tiko3a',
     image: tiko3a,
+    cat: 3
   },
   {
     id: 15,
@@ -125,6 +141,7 @@ export default [
     team: '20tiko3b',
     link: 'https://webpages.tuni.fi/20tiko3b',
     image: tiko3b,
+    cat: 3
   },
   {
     id: 16,
@@ -132,6 +149,7 @@ export default [
     team: '20tiko3c',
     link: 'https://webpages.tuni.fi/20tiko3c',
     image: tiko3c,
+    cat: 3
   },
   {
     id: 17,
@@ -139,6 +157,7 @@ export default [
     team: '20tiko3d',
     link: 'https://webpages.tuni.fi/20tiko3d',
     image: tiko3d,
+    cat: 3
   },
   {
     id: 18,
@@ -146,6 +165,7 @@ export default [
     team: '20tiko3e',
     link: 'https://webpages.tuni.fi/20tiko3e',
     image: tiko3e,
+    cat: 3
   },
   {
     id: 19,
@@ -153,5 +173,6 @@ export default [
     team: '20tiko3f',
     link: 'https://webpages.tuni.fi/20tiko3f',
     image: tiko3f,
+    cat: 3
   },
 ]
