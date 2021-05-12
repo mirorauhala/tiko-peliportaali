a876baba1a57eeb9c3e024af4b882862542b6ba4
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-05-12 14:25:04 +0300

    add winners

--- src/components/TikoWebinar.vue
+++ src/components/TikoWebinar.vue
@@ -1,6 +1,48 @@
 <template>
   <tiko-small-header>Webinaari</tiko-small-header>
   <article class="max-w-4xl mx-auto mb-12 px-3">
+
+    <h2 class="font-bold text-2xl text-tuni mb-4 text-center">Voittajat</h2>
+
+    <div class="mb-3">
+      <h3 class="font-bold text-tuni">Pelitalojen valinta</h3>
+      <p>Kunniamaininnat</p>
+      <ul class="list-disc list-inside mb-3">
+        <li>Labyrinth of life</li>
+        <li>Turn or Fall</li>
+        <li>When the March Ends</li>
+      </ul>
+
+      <p>Voittaja</p>
+      <p><b>Mental Run</b></p>
+    </div>
+
+    <div class="mb-3">
+      <h3 class="font-bold text-tuni">Asiakkaan valinta</h3>
+      <p>Kunniamaininnat</p>
+      <ul class="list-disc list-inside mb-3">
+        <li>Labyrinth of life</li>
+        <li>Tunteiden veto</li>
+        <li>Nightmarescape</li>
+      </ul>
+
+      <p>Voittaja</p>
+      <p><b>Feelin' Ballz</b></p>
+    </div>
+
+    <div class="mb-3">
+      <h3 class="font-bold text-tuni">Yleisöäänestyksen voittaja</h3>
+      <p><b>Mental Survival</b></p>
+      <ul class="list-disc list-inside">
+        <li>Santeri Puttonen</li>
+        <li>Joona Rämö</li>
+        <li>Joel Eklund</li>
+        <li>Edgar Sarelainen</li>
+      </ul>
+    </div>
+
+
+    <h2 class="font-bold text-2xl text-tuni mb-4 text-center">Ohjelma</h2>
     <div class="overflow-auto">
       <table class="table-auto">
         <tr>
